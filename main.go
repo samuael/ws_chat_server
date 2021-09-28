6d05bbba4460ffdaebe5cd69bb9f3730f735d0cf
samuael <samuaelb@umich.edu>
2021-09-29 01:27:51 +0300

    conf

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "80"
+		port = "8070"
 		log.Printf("defaulting to PORT %s", port)
 	}
 	broadcast := &BroadcastChat{
